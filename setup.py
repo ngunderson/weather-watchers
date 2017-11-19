aed0787da9864be8f3269478347ff8d9c56631c8
Nathan Gunderson <gundersonn@uwplatt.edu>
2017-11-19 13:25:27 -0600

    Update setup.py to include wheel dependency
    Signed-off-by: Nathan Gunderson <gundersonn@uwplatt.edu>

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='weather-app',
     version='1.0',
     packages=find_packages(),
-    install_requires=['flask', 'flask-sqlalchemy', 'flask-marshmallow', 'marshmallow-sqlalchemy', 'python-dateutil']
+    install_requires=['flask', 'flask-sqlalchemy', 'flask-marshmallow', 'marshmallow-sqlalchemy', 'python-dateutil', 'wheel']
 )
