3e7acbf3fd60f259f9af5eea3c3bbe6e2b524f33
Nathan Gunderson <npgunderson@gmail.com>
2017-11-29 00:34:01 -0600

    Updated setup.py to include static JS and HTML files when installing
    Signed-off-by: Nathan Gunderson <npgunderson@gmail.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name='weather_server',
     version='1.0',
     packages=find_packages(),
+    package_data={
+        'weather_server': [os.path.join('templates', 'index.html'),
+                           os.path.join('static', 'main.js'),
+                           os.path.join('static', 'jquery-3.2.1.min.js')]
+    },
+    include_package_data=True,
     install_requires=['flask', 'flask-sqlalchemy', 'flask-marshmallow', 'marshmallow-sqlalchemy', 'python-dateutil', 'wheel'],
     extras_require={
         'test': ['flask-testing', 'blinker']
