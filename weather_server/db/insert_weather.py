b4ada02c6208936d1b720e5db7d50a7dbd3a0841
Nathan Gunderson <npgunderson@gmail.com>
2017-11-29 01:40:37 -0600

    Updated imports in db reference scripts
    Signed-off-by: Nathan Gunderson <npgunderson@gmail.com>

--- weather_server/db/insert_weather.py
+++ weather_server/db/insert_weather.py
@@ -1,4 +1,4 @@
-from model import db, Weather
+from weather_server.db.model import db, Weather
 from weather_server.server import create_app
 
 from datetime import datetime, timezone
@@ -6,11 +6,11 @@ from dateutil import tz
 
 create_app().app_context().push()
 
-to_zone = tz.gettz("CST")
+# This file serves as a reference USP for the SQLite DB
 
 weather = Weather(
     device_id=1,
-    time=datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(to_zone),
+    time=datetime.utcnow(),
     temp=60
 )
 
--- weather_server/db/mk_db.py
+++ weather_server/db/mk_db.py
@@ -1,6 +1,7 @@
-from model import db
+from weather_server.db.model import db
 from weather_server.server import create_app
 create_app().app_context().push()
 
+# Script to create initial SQLite DB
 #print 'Uncomment this file to create a new db based on model.py'
-db.create_all()
+#db.create_all()
