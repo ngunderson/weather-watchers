29efd1842d7bd57538c750adeff42797d5b8aaba
Nathan Gunderson <npgunderson@gmail.com>
2017-11-29 01:40:09 -0600

    Update setup.py and README for information on running tests
    Signed-off-by: Nathan Gunderson <npgunderson@gmail.com>

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ setup(
     name='weather_server',
     version='1.0',
     packages=find_packages(),
+    include_package_data=True,
+    test_suite="weather_server.test_server",
     package_data={
         'weather_server': [os.path.join('templates', 'index.html'),
                            os.path.join('static', 'main.js'),
                            os.path.join('static', 'jquery-3.2.1.min.js')]
     },
-    include_package_data=True,
     install_requires=['flask', 'flask-sqlalchemy', 'flask-marshmallow', 'marshmallow-sqlalchemy', 'python-dateutil', 'wheel'],
     extras_require={
         'test': ['flask-testing', 'blinker']
