20d4570ff4a3f446b0b44575f61c4d17afdbe879
Nathan Gunderson <npgunderson@gmail.com>
2017-11-29 01:05:23 -0600

    Add tests for the API views
    Signed-off-by: Nathan Gunderson <npgunderson@gmail.com>

--- weather_server/test_server.py
+++ weather_server/test_server.py
@@ -1,11 +1,14 @@
 from weather_server.server import create_app
-from weather_server.db.model import db
+from weather_server.db.model import db, Device, Weather, DeviceSchema
 from flask_testing import TestCase
 
 import unittest
 import warnings
+import json
 
 # self.client will access the initialized app
+
+devices_schema = DeviceSchema(many=True)
 
 class WeatherServerBase(TestCase):
 
@@ -30,8 +33,235 @@ class TestAPIViews(WeatherServerBase):
         response = self.client.get("/devices")
         self.assertEqual([], response.json)
 
-    #def test_post_device(self):
+    def test_create_valid_device(self):
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "4321",
+            "password": "12345678"
+        })
+        self.assert200(response, "Incorrect Response Code")
 
+        # get last device
+        new_dev = Device.query.order_by(Device.id.desc()).first()
+
+        self.assertEqual(new_dev.latitude, 1234, "Latitude not set correctly")
+        self.assertEqual(new_dev.longitude, 4321, "Latitude not set correctly")
+        self.assertEqual(new_dev.password, "12345678", "Password not set correctly")
+
+        response = self.client.get("/devices")
+
+        self.assertEqual(devices_schema.dump([new_dev]),
+                         devices_schema.load(response.json),
+                         "New device not returned")
+
+    def test_create_multiple_valid_devices(self):
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "4321",
+            "password": "12345678"
+        })
+        self.assert200(response, "Incorrect Response Code")
+
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "4321",
+            "password": "12345678"
+        })
+        self.assert200(response, "Incorrect Response Code")
+
+
+        response = self.client.get("/devices")
+
+        devices = Device.query.all()
+
+        self.assertEqual(2, len(devices))
+        self.assertEqual(devices_schema.dump(devices),
+                         devices_schema.load(response.json))
+
+    def test_create_device_with_invalid_latitude(self):
+        # try with letter and number
+        response = self.client.post("/devices", data={
+            "latitude": "1234a",
+            "longitude": "4321",
+            "password": "12345678"
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+        # try empty
+        response = self.client.post("/devices", data={
+            "latitude": "",
+            "longitude": "4321",
+            "password": "12345678"
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+        # try space only
+        response = self.client.post("/devices", data={
+            "latitude": "  ",
+            "longitude": "4321",
+            "password": "12345678"
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+        # try letters only
+        response = self.client.post("/devices", data={
+            "latitude": "abc",
+            "longitude": "4321",
+            "password": "12345678"
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+
+        devices = Device.query.all()
+
+        self.assertEqual(0, len(devices))
+        self.assertEqual([], devices)
+
+        response = self.client.get("/devices")
+
+        devices = Device.query.all()
+
+        self.assertEqual(0, len(devices))
+        self.assertEqual([], response.json)
+
+    def test_create_device_with_invalid_longitude(self):
+        # try with letter and number
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "4321a",
+            "password": "12345678"
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+        # try empty
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "",
+            "password": "12345678"
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+        # try space only
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "  ",
+            "password": "12345678"
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+        # try letters only
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "abc",
+            "password": "12345678"
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+
+        devices = Device.query.all()
+
+        self.assertEqual(0, len(devices))
+        self.assertEqual([], devices)
+
+        response = self.client.get("/devices")
+
+        devices = Device.query.all()
+
+        self.assertEqual(0, len(devices))
+        self.assertEqual([], response.json)
+
+    def test_create_device_with_invalid_password(self):
+        # try with letters and spaces
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "4321",
+            "password": "1234567   "
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+        # try empty
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "4321",
+            "password": ""
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+        # try space only
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "4321",
+            "password": "   "
+        })
+        self.assert400(response, "Incorrect Response Code")
+
+        devices = Device.query.all()
+
+        self.assertEqual(0, len(devices))
+        self.assertEqual([], devices)
+
+        response = self.client.get("/devices")
+
+        devices = Device.query.all()
+
+        self.assertEqual(0, len(devices))
+        self.assertEqual([], response.json)
+
+    def test_delete_nonexistent_device(self):
+        response = self.client.delete("/devices/0")
+        self.assert404(response)
+
+        response = self.client.delete("/devices/1")
+        self.assert404(response)
+
+        response = self.client.delete("/devices/-1")
+        self.assert404(response)
+
+    def test_delete_device_valid_password(self):
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "4321",
+            "password": "12345678"
+        })
+
+        self.assert200(response, "Incorrect Response Code")
+
+        devices = Device.query.all()
+
+        self.assertEqual(1, len(devices))
+
+        response = self.client.delete("/devices/1", headers={
+            "password" : "12345678"
+        })
+
+        self.assert200(response, "Incorrect Response Code")
+
+        devices = Device.query.all()
+
+        self.assertEqual(0, len(devices))
+
+    def test_delete_device_invalid_password(self):
+        response = self.client.post("/devices", data={
+            "latitude": "1234",
+            "longitude": "4321",
+            "password": "12345678"
+        })
+
+        self.assert200(response, "Incorrect Response Code")
+
+        devices = Device.query.all()
+
+        self.assertEqual(1, len(devices))
+
+        response = self.client.delete("/devices/1", headers={
+            "password" : "mysecretpass"
+        })
+
+        self.assert401(response, "Incorrect Response Code")
+
+        devices = Device.query.all()
+
+        self.assertEqual(1, len(devices))
 
 class TestFrontEndViews(WeatherServerBase):
 
